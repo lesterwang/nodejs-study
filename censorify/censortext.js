f0c3be391e88446f2cf062f9f7b0e8e7d41ab9ce
Lester Wang <wanghuai28@outlook.com>
2015-09-20 11:27:33 +0800

    nodejs censorify

---
+++ censorify/censortext.js
@@ -0,0 +1,24 @@
+var censoredWords = ['sad', 'bad', 'mad'];
+var customCensoredWords = [];
+
+function censor(inStr) {
+	for(var idx in censoredWords) {
+		inStr = inStr.replace(censoredWords[idx], '****');
+	}
+	for(idx in customCensoredWords) {
+		inStr = inStr.replace(customCensoredWords[idx], '****');
+	}
+	return inStr;
+}
+
+function addCensoredWord(word) {
+	customCensoredWords.push(word);
+}
+
+function getCensoredWord() {
+	return censoredWords.concat(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWrod = getCensoredWord;
